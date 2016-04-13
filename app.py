c8f77640d2bb3c2a3823b8cdaf4c6cb2a1530bcb
zagjab <983124300@qq.com>
2016-04-13 22:54:08 +0800

    Revert "Revert "first coommit""
    This reverts commit 6f3af84b78d3052d6a52a65bbae3de4b61340f34.

---
+++ app.py
@@ -0,0 +1,90 @@
+# RESTful API Example
+from flask import Flask, request, redirect,render_template
+import base64
+import random
+import time
+
+app = Flask(__name__)
+
+redirect_uri = "http://localhost:5000/client/passport"
+client_id = '123456'
+users[client_id] = []
+auth_code = {}
+oauth_redirect_uri = []
+
+users = {
+    "zagjab": ["123456"]
+}
+
+def gen_token(uid):
+    token = base64.b64encode(":".join([str(uid), str(random.random()), str(time.time() + 7200)]))
+    users[uid].append(token)
+    return token
+
+def gen_auth_code(uri):
+    code=random.randint(0,10000)
+    auth_code[code]=uri
+    return code
+
+def verify_token(token):
+    _token = base64.b64decode(token)
+    if not users.get(_token.split(':')[0])[-1] == token:
+        return -1
+    if float(_token.split(':')[-1]) >= time.time():
+        return 1
+    else:
+        return 0
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('Hello')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    uid, pw = base64.b64decode(request.headers['Authorization'].split(' ')[-1]).split(':')
+    if users.get(uid)[0] == pw:
+        return gen_token(uid)
+    else:
+        return 'error'
+
+
+@app.route('/oauth', methods=['POST', 'GET'])
+def oauth():
+    if request.args.get('user'):
+        if users.get(request.args.get('user'))[0] ==request.args.get('pw') and oauth_redirect_uri:
+            uri = oauth_redirect_uri[0] + '?code=%s'% gen_auth_code(oauth_redirect_uri[0])
+            return redirect(uri)
+    if request.args.get('code'):
+        if auth_code.get(int(request.args.get('code'))) == request.args.get('redirect_uri'):
+            return gen_token(request.args.get('client_id'))
+    if request.args.get('redirect_uri'):
+        oauth_redirect_uri.append(request.args.get('redirect_uri'))
+    return 'please login'
+
+
+@app.route('/client/login', methods=['POST','GET'])
+def client_login():
+    uri = "http://localhost:5000/oauth?response_type=code&client_id=%s&redirect_uri=%s" %(client_id,redirect_uri)
+    return redirect(uri)
+
+
+@app.route('/client/passport', methods=['POST','GET'])
+def client_passport():
+    code = request.args.get('code')
+    uri = 'http://localhost:5000/oauth?response_type=%s&client_id=%s&redirect_uri=%s' %(code,client_id,redirect_uri)
+    return redirect(uri)
+
+
+@app.route('/test1', methods=['POST', 'GET'])
+def test():
+    token = request.args.get('token')
+    if verify_token(token) == 1:
+        return 'data'
+    else:
+        return 'error'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
